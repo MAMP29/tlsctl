c0edfb8e0ae99cd92d68533d6865818050e2eb75
MAMP29 <106500061+MAMP29@users.noreply.github.com>
2026-01-11 10:24:39 -0500

    Función de escaneo actualizada para que bucle principal funcione mediante el contexto

--- internal/scan/client.go
+++ internal/scan/client.go
@@ -3,6 +3,7 @@ package scan
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log/slog"
 	"math/rand/v2"
@@ -43,71 +44,74 @@ func ScanLogic(ctx context.Context, dom string) (int, ScanTask, error) {
 
 	var scanTask ScanTask
 	var limit500 int
+	var attemptCounter int
 
-	for i := range MaxRetries {
-		slog.Info("Intento numero: ", "intento", i)
-		wait := 5 * time.Second
-		statusCode, err := LaunchExecution(*req, &scanTask)
-		if err != nil {
-			slog.Error("Error al momento de lanzar la ejecución", "err", err)
-			return statusCode, ScanTask{}, err
-		}
+	for {
+		select {
+		case <-ctx.Done():
+			return 0, ScanTask{}, errors.New("Timeout: El escaneo de los endpoints ha superado el limite de tiempo")
+		default:
+			attemptCounter++
+			wait := 5 * time.Second
+			slog.Info("Intento numero: ", "intento", attemptCounter)
+			statusCode, err := LaunchExecution(*req, &scanTask)
+			if err != nil {
+				slog.Error("Error al momento de lanzar la ejecución", "err", err)
+				return statusCode, ScanTask{}, err
+			}
 
-		// SOLUCION DE MANEJO HTTP TEMPORAL A LA ESPERA DE SER MEJORADA
-		switch statusCode {
-		case 400:
-			slog.Error("Parametros invalidos, revisar invocación")
-			errMsg := "Parametros invalidos, revisar invocacion"
-			return statusCode, ScanTask{Status: "failed", Error: &errMsg}, nil
-		case 441:
-			slog.Error("Usuario no autorizado, problema al registrar el correo")
-			errMsg := "Usuario no autorizado, problema al registear el correo"
-			return statusCode, ScanTask{Status: "failed", Error: &errMsg}, nil
-		case 429:
-			slog.Warn("Demasiados intentos, disminuyendo taza de reintentos")
-			// Tambien es necesario consultar cuandos clientes hay activos y permitidos para reducir el numero de trabajadores
-			wait = 30 * time.Second
-		case 500:
-			limit500++
-			slog.Info("Error interno del servicio, reitentando")
-			if limit500 > 4 {
-				slog.Warn("Limite de reintentos sobrepasado, cerrando petición")
-				errMsg := "Error interno del servicio"
+			switch statusCode {
+			case 400:
+				slog.Error("Parametros invalidos, revisar invocación")
+				errMsg := "Parametros invalidos, revisar invocacion"
+				return statusCode, ScanTask{Status: "failed", Error: &errMsg}, nil
+			case 441:
+				slog.Error("Usuario no autorizado, problema al registrar el correo")
+				errMsg := "Usuario no autorizado, problema al registear el correo"
+				return statusCode, ScanTask{Status: "failed", Error: &errMsg}, nil
+			case 429:
+				slog.Warn("Demasiados intentos, atrasando peticion")
+				return statusCode, ScanTask{Status: "pending"}, nil
+			case 500:
+				limit500++
+				slog.Info("Error interno del servicio, reitentando")
+				if limit500 > 4 {
+					slog.Warn("Limite de reintentos sobrepasado, cerrando petición")
+					errMsg := "Error interno del servicio"
+					return statusCode, ScanTask{Status: "failed", Error: &errMsg}, nil
+				}
+				continue
+
+			// USUARIO DEBE REINTENTAR
+			case 503:
+				slog.Warn("Servicio no disponible por el momento, reintentar en 15 minutos")
+				errMsg := "Servicio no disponible, error 503"
+				return statusCode, ScanTask{Status: "failed", Error: &errMsg}, nil
+			case 529:
+				slog.Warn("Servicio sobrecargado, reintentar en 30 minutos")
+				errMsg := "Servicio sobrecargado, error 529"
 				return statusCode, ScanTask{Status: "failed", Error: &errMsg}, nil
 			}
-			continue
 
-		// USUARIO DEBE REINTENTAR
-		case 503:
-			slog.Warn("Servicio no disponible por el momento, reintentar en 15 minutos")
-			errMsg := "Servicio no disponible, error 503"
-			return statusCode, ScanTask{Status: "failed", Error: &errMsg}, nil
-		case 529:
-			slog.Warn("Servicio sobrecargado, reintentar en 30 minutos")
-			errMsg := "Servicio sobrecargado, error 529"
-			return statusCode, ScanTask{Status: "failed", Error: &errMsg}, nil
+			status := scanTask.Status
+			slog.Debug("Estado del progreso en la API: ", "estado", status)
+
+			switch status {
+			case "READY":
+				slog.Debug("Estado de respuesta: ", "estado", statusCode)
+				return statusCode, scanTask, nil
+			case "ERROR":
+				slog.Debug("Error del cliente al momento de procesar el pedido: ", "error", scanTask.Error)
+				return statusCode, scanTask, nil
+			case "IN_PROGRESS":
+				slog.Debug("Esperando a que termine el proceso") // No es factible imprimir el progreso directo del scanTask porque el progreso real se visualiza en los endpoints
+				wait = 10 * time.Second
+			}
+
+			jitter := time.Duration(rand.Float64() * float64(wait) * 0.2)
+			time.Sleep(wait + jitter)
 		}
-
-		status := scanTask.Status
-		slog.Debug("Estado del progreso en la API: ", "estado", status)
-
-		switch status {
-		case "READY":
-			slog.Debug("Estado de respuesta: ", "estado", statusCode)
-			return statusCode, scanTask, nil
-		case "ERROR":
-			slog.Debug("Error del cliente al momento de procesar el pedido: ", "error", scanTask.Error)
-			return statusCode, scanTask, nil
-		case "IN_PROGRESS":
-			slog.Debug("Esperando a que termine el proceso") // No es factible imprimir el progreso directo del scanTask porque el progreso real se visualiza en los endpoints
-			wait = 10 * time.Second
-		}
-
-		jitter := time.Duration(rand.Float64() * float64(wait) * 0.2)
-		time.Sleep(wait + jitter)
 	}
-
-	return 0, ScanTask{}, fmt.Errorf("Ha terminado el numero de intentos maximos, finiquitando proceso")
 }
 
 func LaunchExecution(request http.Request, scanTask *ScanTask) (int, error) {
